8a117f5e22fb6244b48345119560b58d934fa8f5
GuneetAtwal <guneetatwal@gmail.com>
2025-07-16 11:26:59 -0700

    hotdogb: update repo for vendor blobs
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,12 +7,12 @@ git clone --depth=1 https://github.com/Infinity-X-Devices/device_oneplus_sm8150-
 # Kernel
 git clone --depth=1 https://github.com/Infinity-X-Devices/kernel_oneplus_sm8150.git -b 15 kernel/oneplus/sm8150
 
+# Vendor
+git clone --depth=1 https://github.com/Infinity-X-Devices/vendor_oneplus_hotdogb.git -b 15 vendor/oneplus/hotdogb
+git clone --depth=1 https://github.com/Infinity-X-Devices/vendor_oneplus_sm8150-common.git -b 15 vendor/oneplus/sm8150-common 
+
 #Hardware
 git clone --depth=1 https://github.com/GuneetAtwal/android_hardware_oplus.git -b 15 hardware/oplus
-
-# Vendor
-git clone --depth=1 https://github.com/TheMuppets/proprietary_vendor_oneplus_hotdogb.git -b lineage-22.2 vendor/oneplus/hotdogb
-git clone --depth=1 https://github.com/TheMuppets/proprietary_vendor_oneplus_sm8150-common.git -b lineage-22.2 vendor/oneplus/sm8150-common 
 
 export BUILD_USERNAME=guneetatwal
 export BUILD_HOSTNAME=buildbot
